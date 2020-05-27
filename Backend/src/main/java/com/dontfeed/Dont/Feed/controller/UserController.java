6f3c585a4510c6048b9f2381952d7fb5ce5b5f97
Justin Wilson <wilsonjustinwayne@gmail.com>
2020-05-27 00:36:02 -0500

    front end 'created'

--- Backend/src/main/java/com/dontfeed/Dont/Feed/controller/UserController.java
+++ Backend/src/main/java/com/dontfeed/Dont/Feed/controller/UserController.java
@@ -22,7 +22,7 @@ public class UserController {
     private final UserService userService;
 
     // full path is "/api/users"
-    @GetMapping("s")
+    @GetMapping("/getAll")
     public ResponseEntity<?> getAllUsers() {
         List<User> users = userService.findAllUsers();
         if (users == null) {
