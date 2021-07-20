b9ad4b52445d58fe4f2fe3afe078913f2fa59b4f
Justin Wilson <wilsonjustinwayne@gmail.com>
2021-07-20 00:04:52 -0500

    updating schema.sql

--- Backend/src/main/resources/schema.sql
+++ Backend/src/main/resources/schema.sql
@@ -55,6 +55,13 @@ create table leagues_contacts
         foreign key (league_id) references leagues (id)
 );
 
+create table roles
+(
+    id   int auto_increment
+        primary key,
+    name varchar(20) null
+);
+
 create table teams
 (
     id             bigint auto_increment
@@ -63,13 +70,18 @@ create table teams
     logo_file_path varchar(255) null,
     name           varchar(255) null,
     password       varchar(255) null,
-    team_info      varchar(255) null
+    team_info      varchar(255) null,
+    last_updated   date         null
 );
 
 create table teams_leagues
 (
-    team_id   bigint not null,
-    league_id bigint not null,
+    team_id       bigint       not null,
+    league_id     bigint       not null,
+    id            bigint       not null,
+    original_name varchar(255) null,
+    placement     int          not null,
+    win_loss      varchar(255) null,
     constraint FKfucxjkr7xb2ceuitn9fntutci
         foreign key (league_id) references leagues (id),
     constraint FKngtnfj47xx3m8pma7xj4y99qc
@@ -118,6 +130,7 @@ create table matches
     team_a_id       bigint       null,
     team_b_id       bigint       null,
     tournament_id   bigint       null,
+    map             varchar(255) null,
     constraint FK23dnop04r2pfj2wvo21vakpph
         foreign key (league_id) references leagues (id),
     constraint FKeeniokyjgo5k6rmhjujatn27i
@@ -156,13 +169,40 @@ create table users
         primary key,
     birth_date            date         null,
     date_joined           date         null,
-    email_address         varchar(255) null,
+    email                 varchar(255) null,
     first_name            varchar(255) null,
     last_name             varchar(255) null,
     last_updated          date         null,
     password              varchar(255) null,
     profile_pic_file_path varchar(255) null,
-    username              varchar(255) null
+    username              varchar(255) null,
+    roles                 tinyblob     null,
+    constraint UKr43af9ap4edm43mmtq01oddj6
+        unique (username)
+);
+
+create table matches_players
+(
+    id       bigint auto_increment
+        primary key,
+    team     char   not null,
+    match_id bigint null,
+    user_id  bigint null,
+    constraint FK48p68lls9tbbtgbs970s1paqh
+        foreign key (user_id) references users (id),
+    constraint FKk0ikmaed5w7b5vmdbw42nuy3l
+        foreign key (match_id) references matches (id)
+);
+
+create table user_roles
+(
+    user_id bigint not null,
+    role_id int    not null,
+    primary key (user_id, role_id),
+    constraint FKh8ciramu9cc9q3qcqiv4ue8a6
+        foreign key (role_id) references roles (id),
+    constraint FKhfh9dx7w3ubf1co1vdev94g3f
+        foreign key (user_id) references users (id)
 );
 
 create table users_games
@@ -177,8 +217,12 @@ create table users_games
 
 create table users_leagues
 (
-    user_id   bigint not null,
-    league_id bigint not null,
+    user_id   bigint       not null,
+    league_id bigint       not null,
+    id        bigint       not null,
+    placement int          not null,
+    team      varchar(255) null,
+    win_loss  varchar(255) null,
     constraint FK5q79fp6tjbc6a9307xiipcfmt
         foreign key (user_id) references users (id),
     constraint FKno0mrvrh90a0lao3tib4p5ema
