e35ba1e1fb4d673b8e9ebe11732e425fb7900919
Justin Wilson <wilsonjustinwayne@gmail.com>
2021-08-17 16:33:38 -0500

    Moved Test To LeagueCreationPage

--- frontend/src/components/LeagueCreationPage/LeagueCreationPage.jsx
+++ frontend/src/components/LeagueCreationPage/LeagueCreationPage.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import FormTextInput from "../utils/Form/FormTextInput";
-import FormSelectInput from "../utils/Form/FormSelectInput";
-import FormTextAreaInput from "../utils/Form/FormTextAreaInput";
-import FormSubmitButton from "../utils/Form/FormSubmitButton";
 import Row from "react-bootstrap/Row"
 import { connect } from "react-redux";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function LeagueCreationPage(props) {
 	const { auth, league } = props;
@@ -17,99 +17,114 @@ function LeagueCreationPage(props) {
 		options: ["Random Every Week", "Performance Based", "Predetermined"],
 		values: ["RANDOMEVERYWEEK", "PERFORMANCEBASED", "PREDETERMINED"]
 	}
+
 	const games = {
 		options: ["Dota 2", "League of Legends", "Super Smash Bros. Ultimate", "Overwatch", "Madden 21"],
 		values: ["DOTA2", "LEAGUEOFLEGENDS", "SMASHBROSULTIMATE", "OVERWATCH", "MADDEN21"]
 	}
+	
 	return (
-		<div className="app-container container-fluid df-dark-background-2">
-			<div className="container-fluid page-container">
-				<Row>
-					<div className="col s12">
-						<h1 className="page-title">CREATE A LEAGUE</h1>
-					</div>
-				</Row>
-				<div className="row">
-					<form className="col s12" onSubmit={e => {
-					e.preventDefault()}
-					}>
-						<div className="row">
-							<div className="input-field col s6">
-								<FormTextInput type="text" label="Logo" id="logo" name="logo"
-											   value={props.logo} handleChange={this.handleChange}
-											   required={false} disabled={true} />
-							</div>
-							<div className="col s2" />
-							<div className="col s1">
-								<input style={{ display: "none" }} type="file"
-									   ref={fileInput => this.fileInput = fileInput} />
-								<button className="grey-btn btn right" onClick={(e) => {
-									e.preventDefault();
-									this.fileInput.click();
-								}}>Browse
-								</button>
-							</div>
-						</div>
-						<div className="row">
-							<div className="input-field col s12">
-								<FormTextInput type="text" label="League Name" id="name" name="name"
-											   value={this.state.name} handleChange={this.handleChange}
-											   required={true} disabled={false} />
-							</div>
-						</div>
-						<div className="row">
-							<div className="input-field col s6">
-								<FormSelectInput label="League Format" id="league_format" name="format"
-												 value={this.state.format} handleChange={this.handleChange}
-												 options={leagueFormat.options} values={leagueFormat.values}
-												 required={true} />
-							</div>
-							<div className="input-field col s6">
-								<FormSelectInput label="Game" id="game" name="game"
-												 value={this.state.game} handleChange={this.handleChange} options={games.options}
-												 values={games.values} required={true} />
-							</div>
-						</div>
-						<div className="row">
-							<div className="input-field col s6">
-								<FormTextInput type="date" label="Start Date" id="start_date"
-											   name="startDate"
-											   value={this.state.startDate} handleChange={this.handleChange}
-											   required={true} />
-							</div>
-							<div className="input-field col s6">
-								<FormTextInput type="date" label="End Date" id="end_date" name="endDate"
-											   value={this.state.endDate} handleChange={this.handleChange}
-											   required={true} />
-							</div>
-						</div>
-						<div className="row">
-							<div className="input-field col s6">
-								<FormTextInput type="password" label="Password" id="leaguePassword"
-											   name="leaguePassword"
-											   value={this.state.leaguePassword}
-											   handleChange={this.handleChange}
-											   required={true} />
-							</div>
-							<div className="input-field col s6">
-								<FormTextInput type="number" label="Match Frequency" id="match_frequency"
-											   name="matchFrequency" value={this.state.matchFrequency}
-											   handleChange={this.handleChange} required={true}
-								/>
-							</div>
-						</div>
-						<div className="row">
-							<div className="input-field col s12">
-								<FormTextAreaInput label="Rules" id="rules" name="rules"
-												   value={this.state.rules} handleChange={this.handleChange}
-												   required={false} />
-							</div>
-						</div>
-						<FormSubmitButton />
-					</form>
-				</div>
-			</div>
-		</div>
+		<Container fluid className="app-container df-dark-background-2">
+			<Row>
+				<Col className="my-5">
+					<h1 className="page-title">CREATE A LEAGUE</h1>
+				</Col>
+			</Row>
+			<Row className="px-5">
+				<Form onSubmit={event => {
+					event.preventDefault();
+					//todo redux the league actions/reducers
+				}}>
+					<Row>
+						<Form.Group as={Col} className="mb-3" controlId="leagueCreateName">
+							<Form.Label className="ms-3">League Name</Form.Label>
+							<Form.Control type="text" placeholder="League Name" />
+						</Form.Group>
+						<Form.Group as={Col} className="mb-3" controlId="leagueCreateLogo">
+							<Form.Label className="ms-3">Logo</Form.Label>
+							<Form.Control type="file" onChange={() => {
+								//todo create functionality for uploading a image
+							}} />
+						</Form.Group>
+					</Row>
+					<Row>
+						<Form.Group as={Col} className="mb-3" controlId="leagueCreateGame">
+							<Form.Label>Game</Form.Label>
+							<Form.Select>
+								<option value={"DOTA2"}>Dota 2</option>
+								<option value={"LEAGUEOFLEGENDS"}>League of Legends</option>
+								<option value={"SMASHBROSULTIMATE"}>Smash Bros. Ultimate</option>
+								<option value={"OVERWATCH"}>Overwatch</option>
+								<option value={"MADDEN21"}>Madden 21</option>
+							</Form.Select>
+						</Form.Group>
+						<Form.Group as={Col} className="mb-3" controlId="leagueCreateFormat">
+							<Form.Label>League Format</Form.Label>
+							<Form.Select>
+								<option value={"RANDOMEVERYWEEK"}>Random Every Week</option>
+								<option value={"PERFORMANCEBASED"}>Performance Based</option>
+								<option value={"PREDETERMINED"}>Predetermined</option>
+							</Form.Select>
+						</Form.Group>
+						{/* todo only show this if format is not set to PREDETERMINED*/}
+						<Form.Group as={Col} className="mb-3" controlId="leagueCreateMatchFrequencyMatches">
+							<Form.Label>Number of matches</Form.Label>
+							<Form.Select>
+								<option value={1}>1</option>
+								<option value={2}>2</option>
+								<option value={3}>3</option>
+								<option value={4}>4</option>
+								<option value={5}>5</option>
+								<option value={6}>6</option>
+							</Form.Select>
+						</Form.Group>
+						<Form.Group as={Col} className="mb-3" controlId="leagueCreateMatchFrequencyMatchesPer">
+							<Form.Label as="legend" column sm={2}>
+								per
+							</Form.Label>
+							<Form.Check
+								inline
+								type="radio"
+								label="Day"
+								name="leagueCreateMatchFrequencyPerRadios"
+								id="leagueCreateMatchFrequencyPerDay"
+							/>
+							<Form.Check
+								inline
+								type="radio"
+								label="Week"
+								name="leagueCreateMatchFrequencyPerRadios"
+								id="leagueCreateMatchFrequencyPerWeek"
+							/>
+						</Form.Group>
+					</Row>
+					<Row>
+						{/* todo turn into date pickers*/}
+						<Form.Group as={Col} className="mb-3" controlId="leagueCreateStartDate">
+							<Form.Label className="ms-3">Start Date</Form.Label>
+							<Form.Control type="text" placeholder="Start Date" />
+						</Form.Group>
+						<Form.Group as={Col} className="mb-3" controlId="leagueCreateEndDate">
+							<Form.Label className="ms-3">End Date</Form.Label>
+							<Form.Control type="text" placeholder="End Date" />
+						</Form.Group>
+						<Form.Group as={Col} className="mb-3" controlId="leagueCreatePassword">
+							<Form.Label className="ms-3">Password</Form.Label>
+							<Form.Control type="password" placeholder="Password" />
+						</Form.Group>
+					</Row>
+					<Row>
+						<Form.Group className="mb-3" controlId="leagueCreateRules">
+							<Form.Label>Rules</Form.Label>
+							<Form.Control as="textarea" rows={5} />
+						</Form.Group>
+					</Row>
+					<Button size="lg" variant="primary" type="submit">
+						Create League
+					</Button>
+				</Form>
+			</Row>
+		</Container>
 	);
 }
 
--- frontend/src/components/utils/Form/FormTitle.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function FormTitle({title}) {
-    return(
-        <div className="col s12">
-            <h1 className="page-title">{title}</h1>
-        </div>
-    )
-}
